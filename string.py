7684e7b644f54862f92fb709146aeff643ec0b83
sanjevk <sanjevkec@gmail.com>
2019-06-09 13:56:47 +0530

    String datatype learnings

---
+++ string.py
@@ -0,0 +1,23 @@
+x="  _Sanjev!  "
+y="24"
+z="Professional"
+print(x)
+print(y)
+print(z)
+
+print(x+ " " + y+ " " + z)
+
+a="5"
+"""if a is declared as int, python will throw error, change into string and run, it will run"""
+print(x+a)
+
+"""String functions"""
+print(len(x))
+print(x.upper())
+print(z.lower())
+print(z.split("s"))
+print(x.strip())
+print(z.replace("P", "f"))
+
+x=input("Enter the user name")
+print("The user name entered is: " + x)
