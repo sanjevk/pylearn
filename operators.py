8c025bb802e2abaaee58522606861f5b4f67ffb4
sanjevk <sanjevkec@gmail.com>
2019-06-09 16:23:47 +0530

    Python operators learnings

--- operators.py
+++ operators.py
@@ -16,3 +16,36 @@ print(c)
 print(d)
 print(e)
 print(f)
+
+"""Assignment operators"""
+l=3
+m=4
+l+=l
+print(l)
+print(l%m)
+
+"""Comparison operators"""
+
+c=5
+d=6
+e=7
+print(c>d)
+print(d>e)
+print(e>c)
+print(c>d and e>c)
+print(c>d or e>c)
+
+"""Identity operators"""
+
+C=10
+D=10.5
+print(C is D)
+print(C is not D)
+
+"""Membership operators"""
+
+x="Sanjev"
+y="a"
+
+print(y in x)
+print(x in y)
